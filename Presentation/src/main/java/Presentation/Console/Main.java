1c3402ae18274c43a6774871402df4b8ca8d64c4
Alexander Kim <limosha@inbox.ru>
2025-04-03 10:17:54 +0300

    fix: repaired Flyway migration

--- Presentation/src/main/java/Presentation/Console/Main.java
+++ Presentation/src/main/java/Presentation/Console/Main.java
@@ -2,9 +2,12 @@ package Presentation.Console;
 
 
 import Presentation.Interfaces.IMenu;
+import org.flywaydb.core.Flyway;
 
 public class Main {
     public static void main(String[] args) {
+        Flyway flyway = Flyway.configure().dataSource("jdbc:postgresql://localhost:5432/JavaLabsDb", "postgres", "limosha").load();
+        flyway.migrate();
         IMenu menu = new Menu();
         menu.Run();
     }
